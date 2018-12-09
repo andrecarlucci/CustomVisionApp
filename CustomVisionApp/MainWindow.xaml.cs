04e20840b4b90e97f07538b4d6a3a381f2ddddc2
andrecarlucci <andrecarlucci@gmail.com>
2018-12-09 19:46:00 -0200

    Load image

--- CustomVisionApp/MainWindow.xaml.cs
+++ CustomVisionApp/MainWindow.xaml.cs
@@ -1,26 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CustomVisionApp.Models;
+using Microsoft.Win32;
+using OpenCvSharp;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CustomVisionApp {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow()
-        {
+    public partial class MainWindow : System.Windows.Window {
+
+        private MyCamera _camera;
+
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void BStart_Click(object sender, RoutedEventArgs e) {
+            if(bStart.Content.Equals("Start")) {
+                _camera = new MyCamera();
+                _camera.NewImage = NewImage;
+                bStart.Content = "Stop";
+                bLoad.IsEnabled = false;
+                Task.Factory.StartNew(() => _camera.StartCamera(0));
+            }
+            else {
+                _camera.Dispose();
+                bStart.Content = "Start";
+                bLoad.IsEnabled = true;
+            }
+        }
+
+        private void NewImage(Mat frame) {
+            var stream = frame.ToMemoryStream(".png");
+            var bitmap = new Bitmap(stream);
+            ChangeUI(() => {
+                var source = ConvertToBitmapSource(bitmap);
+                TheImage.Source = source;
+                TheWindow.Title = "Azure Vision: " + _camera.FramesPerSecond.ToString();
+            });
+        }
+
+        private void ChangeUI(Action action) {
+            Dispatcher.BeginInvoke(action);
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ConvertToBitmapSource(Bitmap bmp) {
+            var handle = bmp.GetHbitmap();
+            try {
+                return Imaging.CreateBitmapSourceFromHBitmap(
+                    handle,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        private void BDescribe_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        private void BLoad_Click(object sender, RoutedEventArgs e) {
+            var op = new OpenFileDialog {
+                Title = "Select a picture",
+                Filter = @"All supported graphics|*.jpg;*.jpeg;*.png|
+                         JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|
+                         Portable Network Graphic (*.png)|*.png"
+            };
+            if (op.ShowDialog() == true) {
+                TheImage.Source = new BitmapImage(new Uri(op.FileName));
+            }
         }
     }
 }
---
+++ CustomVisionApp/Models/MyCamera.cs
@@ -0,0 +1,45 @@
+﻿using OpenCvSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomVisionApp.Models {
+    public class MyCamera : IDisposable {
+        private bool _running;
+        private object _sync = new object();
+        private VideoCapture _capture;
+        public Action<Mat> NewImage;
+
+        private DateTime _lastFrame;
+        public double FramesPerSecond { get; set; }
+
+        public async Task StartCamera(int delay = 0) {
+            _running = true;
+            _capture = new VideoCapture(0);
+            _lastFrame = DateTime.Now;
+            while (_running) {
+                lock (_sync) {
+                    if (_capture?.IsDisposed ?? true) {
+                        return;
+                    }
+                    using (var mat = new Mat()) {
+                        _capture.Read(mat);
+                        NewImage?.Invoke(mat.Clone());
+                    }
+                    FramesPerSecond = 1 / (DateTime.Now - _lastFrame).TotalSeconds;
+                    _lastFrame = DateTime.Now;
+                }
+                if (_running && delay > 0) {
+                    await Task.Delay(delay);
+                }
+            }
+        }
+
+        public void Dispose() {
+            lock (_sync) {
+                _running = false;
+                _capture?.Dispose();
+                _capture = null;
+            }
+        }
+    }
+}
